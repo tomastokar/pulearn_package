f31f213de5f3acc167e7a472460b383a49794d67
Tomas Tokar <tomastokar@Tomass-MacBook-Pro.local>
2019-07-22 17:02:13 -0400

    Induction

--- pulearn/pu.py
+++ pulearn/pu.py
@@ -34,3 +34,24 @@ def bagging(mod, X, Y, iters = 100, r = 0.1):
     return(proba)
 
 
+def induction(mod, X, Y, rel_neg = [], iters = 100):
+    '''
+    Function performs PU inductions (aka. 2-step method)
+    '''
+    p, u = [], []
+    for i, y in enumerate(Y):
+        if y == 1:
+            p.append(i)
+        else:
+            u.append(i)
+
+    n = rel_neg
+    for i in range(iters):
+        proba = (
+            mod
+            .fit(X[p + n], Y[p + n])
+            .predict_proba(X)[:,1]
+        )        
+        n = [j for j in u if proba[j] < .5]
+    
+    return proba
